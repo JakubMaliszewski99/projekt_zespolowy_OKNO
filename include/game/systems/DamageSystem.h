51d8801f84931af574f4f16ebfb14ba80e49f35f
KrzysiekBlankiewicz <krzysiek.wiggin.blankiewicz@gmail.com>
2024-05-25 18:50:20 +0200

    methods in DamageSystem

--- include/game/systems/DamageSystem.h
+++ include/game/systems/DamageSystem.h
@@ -2,7 +2,11 @@
 
 #include "../../core/ecs/System.h"
 #include "../../core/engine/InputManager.h"
-
+#include "../components/TransformComponent.h"
+#include "../components/HealthComponent.h"
+#include <math.h>
+#include <vector>
+#include <utility>
 #include <iostream>
 
 class DamageSystem : public System{
@@ -14,11 +18,13 @@ public:
         for (auto const &entity : m_entities) {
             auto &weapon =
                 m_manager->getComponent<WeaponComponent>(entity);
+            auto &transform = 
+                m_manager->getComponent<TransformComponent>(entity);
 
             if(weapon.fired == true) {
                 weapon.fired = false;
                 if(weapon.activeWeaponType == WeaponType::eRangeWeapon) {
-                    this->hitscanFire();
+                    this->hitscanFire(transform);
                 }
                 else if (weapon.activeWeaponType == WeaponType::eHandWeapon) {
                     this->meeleFire();
@@ -35,12 +41,41 @@ private:
 
     }
 
-    void hitscanFire(){
-        //TODO - hitscan...
+    void hitscanFire(TransformComponent& shooterTransform){
+        //TODO below code is ignoring walls:
+        std::vector<std::pair<Entity, float>> entitiesToBeHit;
+
+        for (auto const &entity : m_entities) {
+            auto &healthComp =
+                m_manager->getComponent<HealthComponent>(entity);
+            auto &transform = 
+                m_manager->getComponent<TransformComponent>(entity);
+            
+            //TODO check if entity would be hit
+            //TODO if so, add it to vector
+        }
+        //TODO find closest entity from vector
+        //TODO decrement health
     }
 
     void projectileFire() {
         //TODO - projectile launching
+    }
+
+    float distanceFromLineOfHitscan(TransformComponent& shooterTransform, TransformComponent& targetTransform){
+
+    }
+
+    float angleBetweenPoints(float x1, float y1, float x2, float y2){
+        return 0;
+    }
+
+    float distanceBetweenPoints(float x1, float y1, float x2, float y2) {
+        float x = x1 - x2;
+        float y = y1 - y2;
+        float distance = sqrtf(x * x + y * y);
+
+        return distance;
     }
 
     std::shared_ptr<ECSManager> m_manager;
